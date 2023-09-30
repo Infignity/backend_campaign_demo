6a44b48c34e989e395d94b12ab5f7b620e2e49f7
wrefinity <wrefordmessi@gmail.com>
2023-09-30 10:14:38 +0100

    changing celery path

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -14,8 +14,8 @@ DEBUG = os.environ.get('DEBUG') == 'TRUE',
 ALLOWED_HOSTS = ['*']
 
 # BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = os.environ.get('REDIS_URL')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
